7b100b051d47b87c49157b9e7367051394320c41
BirdMilk <oenibe@gmail.com>
2024-01-07 14:22:39 -0800

    05 done

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my = my_list
+    if idx < 0:
+        return my
+    elif idx >= len(my_list):
+        return my
+    else:
+        my[idx] = element
+        return my
