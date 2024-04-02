fc612e74f6a1559d2f4008272815075ceaf86139
BirdMilk <oenibe@gmail.com>
2024-04-02 20:19:03 +0100

    done with exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to raise a type exception
+# demonstrates how to raise exception
+
+
+def raise_exception():
+    raise TypeError
